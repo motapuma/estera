c54fa66113d4a79002d6a313667e5f4df59ec5ff
Alberto Mota <mota_al@hotmail.com>
2015-05-16 18:48:05 -0500

    ya pueden agregar compradores

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -15,4 +15,30 @@ class ServicesController < ApplicationController
 		@service = Service.new
 	end
 
+	def create
+
+		  @service = Service.new(candidate_params)
+
+		  respond_to do |format|
+		    if @service.save
+		      format.html { redirect_to :back, notice: 'Servicio Generado' }
+		      format.json { render :show, status: :created, location: @candidate }
+		    else
+		      format.html { render :announce }
+		      format.json { render json: @candidate.errors, status: :unprocessable_entity }
+		    end
+		  end
+		
+	end
+
+	def index
+		@services = Service.all
+	end
+
+	private
+
+	def candidate_params
+      params.require(:service).permit(:name,:latitude,:longitude,:phone,:email,:type,urls_attributes: [:url,:service_id])
+    end
+
 end
